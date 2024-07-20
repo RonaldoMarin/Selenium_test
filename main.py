91b709189869e6052d4e87c24f90580874659b30
RonaldoMarin <marinhoronaldo02@gmail.com>
2024-07-20 17:49:23 -0300

    feat: mudança do get kkkkk

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from setup import Setup
 
 class SeleniumTest(Setup):
     def abrir(self):
-        self.driver.get("https://github.com/RonaldoMarin/Selenium_test")
+        self.driver.get("https://youtu.be/dQw4w9WgXcQ?si=uqLjhgaF6pHj8CQ1")
         input()
         
 
