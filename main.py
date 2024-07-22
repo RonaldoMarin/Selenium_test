0ae17ab52031f7f23b19f4d008049c60f342141a
RonaldoMarin <marinhoronaldo02@gmail.com>
2024-07-22 13:58:41 -0300

    l

--- main.py
+++ main.py
@@ -4,8 +4,6 @@ class SeleniumTest(Setup):
     def abrir(self):
         self.driver.get("https://youtu.be/dQw4w9WgXcQ?si=uqLjhgaF6pHj8CQ1")
         input()
-        
-
 
 if __name__ == "__main__":
     main = SeleniumTest()
