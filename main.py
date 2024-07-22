6afc9542e7ffc3c20bdd7b037b11b6b9ce52a6ab
RonaldoMarin <marinhoronaldo02@gmail.com>
2024-07-22 17:09:49 -0300

    f

--- main.py
+++ main.py
@@ -1,39 +1,45 @@
+from time import sleep
 from setup import Setup
 from selenium.webdriver.common.by import By
-from time import sleep
-
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
 
 class SeleniumTest(Setup):
 
-    input_login_suap = (By.NAME, 'username')
-    input_senha_suap = (By.NAME, 'password')
-    botao_acessar_suap = (By.CLASS_NAME, 'submit-row')
-    botao_ensino_suap = (By.CLASS_NAME, 'menu-ensino')
-    botao_boletins_suap = (By.CSS_SELECTOR, '#menu-item-ensino_boletins > a')
-    botao_documentos_suap = (By.CSS_SELECTOR, '#content > div.title-container > div.action-bar-container > ul > li:nth-child(2) > a')
-    botao_declaracao_de_vinculo = (By.CSS_SELECTOR, '#content > div.title-container > div.action-bar-container > ul > li:nth-child(2) > ul > li:nth-child(1) > a')
+    input_login_github = (By.ID, 'login_field')
+    botao_login_github = (By.CSS_SELECTOR, '#login > div.auth-form-body.mt-3 > form > div > input.btn.btn-primary.btn-block.js-sign-in-button')
+    botao_new_github = (By.CSS_SELECTOR, 'body > div.logged-in.env-production.page-responsive.full-width > div.application-main > div > div > aside > div > div > loading-context > div > div.Details.js-repos-container > div > div.hide-sm.hide-md.mb-1.d-flex.flex-justify-between.flex-items-center > a')
+    input_repository_name_github = (By.CSS_SELECTOR, '#\:rb\:')
+    botao_criar_repositorio_github = (By.XPATH, '/html/body/div[1]/div[5]/main/react-app/div/form/div[4]/div[1]/div[1]/div[1]/fieldset/div/div[2]/div/span/input')
 
 
     def abrir(self):
-        self.driver.get("https://suap.ifrn.edu.br/accounts/login/?next=/")
+        self.driver.get("https://github.com/login")
 
-    def logar_no_suap(self):
-        login = "20211014040044"
-        self.driver.find_element(*self.input_login_suap).send_keys(login)
-        self.driver.find_element(*self.input_senha_suap)
+    def logar_no_github(self):
+        login = "ronaldoMarin"
+        self.driver.find_element(*self.input_login_github).send_keys(login)
         input()
-        self.driver.find_element(*self.botao_acessar_suap).click()
+        self.driver.find_element(*self.botao_login_github).click()
         print(1)
 
-    def navegar_ate_aba_boletim(self):
-        self.driver.find_element(*self.botao_ensino_suap).click()
+    def criar_repositorio(self, nome_repositorio:str):
+        self.driver.find_element(*self.botao_new_github).click()
         sleep(0.3)
-        self.driver.find_element(*self.botao_boletins_suap).click()
+
+        # WAITS
+        WebDriverWait(self.driver, 20).until(
+            ec.element_to_be_clickable((By.ID, ':r7:'))
+        ).send_keys(nome_repositorio)
+
+        self.driver.find_element(*self.botao_criar_repositorio_github).click()
         sleep(2)
 
 
 if __name__ == "__main__":
     main = SeleniumTest()
     main.abrir()
-    main.logar_no_suap()
+    main.logar_no_github()
+    nome_repositorio = "TESTE_E_LEGAL"
+    main.criar_repositorio(nome_repositorio)
     input()
